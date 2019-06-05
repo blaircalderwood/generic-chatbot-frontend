e96e0385c98e9368ec68853da243f09595a068fe
Blair Calderwood <blaircalderwood@live.co.uk>
2019-06-05 15:48:37 +0100

    Added basic chatbot structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewMessageInputComponent } from './components/new-message-input/new-message-input.component';
+import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { SentMessageComponent } from './components/sent-message/sent-message.component';
+import { ReceivedMessageComponent } from './components/received-message/received-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewMessageInputComponent,
+    ChatWindowComponent,
+    SentMessageComponent,
+    ReceivedMessageComponent
   ],
   imports: [
     BrowserModule
